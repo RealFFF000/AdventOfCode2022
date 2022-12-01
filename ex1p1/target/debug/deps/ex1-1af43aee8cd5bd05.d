981488cb15089420774b2e12cef6df94c56efa2d
rafau <adas.rafalowski@gmail.com>
2022-12-01 20:04:52 +0100

    uwu5

--- ex1p1/target/debug/deps/ex1-1af43aee8cd5bd05.d
+++
@@ -1,5 +0,0 @@
-/home/adam/Code/AdventOfCode2022/ex1/target/debug/deps/ex1-1af43aee8cd5bd05: src/main.rs
-
-/home/adam/Code/AdventOfCode2022/ex1/target/debug/deps/ex1-1af43aee8cd5bd05.d: src/main.rs
-
-src/main.rs:
--- ex1p2/target/debug/ex1.d
+++
@@ -1,1 +0,0 @@
-/home/adam/Code/AdventOfCode2022/ex1/target/debug/ex1: /home/adam/Code/AdventOfCode2022/ex1/src/main.rs
